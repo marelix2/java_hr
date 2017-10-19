5f1fbbc9ff20a12d8460f677064e5c48c39530ed
marelix2 <marelix2@gmail.com>
2017-10-19 23:26:14 +0200

    zad4

---
+++ zad4/src/zad4.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Michał on 19.10.2017.
+ */
+public class zad4 {
+
+    public static void main(String args[]){
+
+        System.out.print(Math.nextUp(Double.MAX_VALUE));
+        System.out.print(Math.nextUp(0));
+        
+    }
+}
